d6ae781ae3011b00d7367558b6aafa47c07451ac
Chintan Patel <chintan.patel@qdata.io>
2016-08-31 09:58:16 +0530

    Remove meta data index

--- mongo_connector/doc_managers/elastic2_doc_manager.py
+++ mongo_connector/doc_managers/elastic2_doc_manager.py
@@ -151,12 +151,8 @@ class DocManager(DocManagerBase):
             "_ts": timestamp
         }
         # Index the source document, using lowercase namespace as index name.
-        self.elastic.index(index=index, doc_type=doc_type,
+        self.elastic.index(index=self.meta_index_name, doc_type=doc_type,
                            body=self._formatter.format_document(doc), id=doc_id,
-                           refresh=(self.auto_commit_interval == 0))
-        # Index document metadata with original namespace (mixed upper/lower).
-        self.elastic.index(index=self.meta_index_name, doc_type=self.meta_type,
-                           body=bson.json_util.dumps(metadata), id=doc_id,
                            refresh=(self.auto_commit_interval == 0))
         # Leave _id, since it's part of the original document
         doc['_id'] = doc_id
